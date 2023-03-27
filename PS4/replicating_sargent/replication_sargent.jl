cf72f6a16a6823a86addec29d96c04457f881d20
Luan Borelli <35273857+luanborelli@users.noreply.github.com>
2023-03-26 22:54:36 -0300

    Update replication_sargent.jl

--- PS4/replicating_sargent/replication_sargent.jl
+++ PS4/replicating_sargent/replication_sargent.jl
@@ -2,7 +2,7 @@
 ## Replicating Sargent's 18.7. computed examples       ##
 #########################################################
 
-# Figs 18.6.3 and 18.7.2 approximately
+# Replicating figures 18.6.3 and 18.7.2.
 
 
 ###############################
@@ -11,9 +11,9 @@
 
 using Plots, Distributions, Random, Base.Threads, LinearAlgebra, Roots, SparseArrays
 
-##################################################################
-## Defining functions to be used in the entire rest of the code ## 
-##################################################################
+###################################################################
+## Defining functions to be used thorughout the rest of the code ## 
+###################################################################
 
 #######################################
 ## Tauchen's discretization function ##
@@ -55,26 +55,6 @@ function tauchen(μ,σsq,ρ,N,m)
     return θ, P    
 end
 
-# The following function takes a block matrix as input and returns the equivalent non-partioned matrix: 
-
-function blockmatrix_to_matrix(A::Array{Array{T,2},2}) where T
-    nblocks = size(A)
-    nrows, ncols = size(A[1,1])
-    nrows_total = nrows * nblocks[1]
-    ncols_total = ncols * nblocks[2]
-    B = zeros(T, nrows_total, ncols_total)
-    for i in 1:nblocks[1]
-        for j in 1:nblocks[2]
-            rowstart = (i-1) * nrows + 1
-            rowend = rowstart + nrows - 1
-            colstart = (j-1) * ncols + 1
-            colend = colstart + ncols - 1
-            B[rowstart:rowend, colstart:colend] = A[i,j]
-        end
-    end
-    return B
-end
-
 # Given an interest rate 'r' as input, the following function solves the individuals' problem and returns
 # i) 'agg_savings': the aggregate savings of the economy;
 # ii) 'V': the value function;
@@ -85,48 +65,20 @@ end
 
 function solve_individuals_problem(r)
 
-    #######
-    # VFI #
-    #######
+    ######################################################################
+    # VFI: Exploiting monotonicity + concavity and using the accelerator #
+    ######################################################################
 
     tol = 10^(-5); # Tolerance for the distance between elements of the value function.
     V_prev = ones(length(a_grid), length(z_grid)); # Temporary vector for the value function iteration.
     policy = zeros(length(a_grid), length(z_grid)); # Vector that will store the policy function.
     policy_index = Int.(zeros(length(a_grid), length(z_grid))); # Vector that will store the indexes of the policy function.
     values = zeros(length(a_grid), length(z_grid)); # Vector that will store the Bellman equation values during the brute-force grid-search. 
-    V = zeros(length(a_grid), length(z_grid)); # Initial guess for the value function.
+    # V = zeros(length(a_grid), length(z_grid)); # Initial guess for the value function.
+    V = [u(r*a_grid[i] + exp(z_grid[j]))./(1-β) for i in eachindex(a_grid), j in eachindex(z_grid)] # Moll's initial guess
+
     iter = 0; # We will use this variable to count the number of iterations.
     maxiter = 100000; # Maximum number of iterations. We will use this to avoid the possibility of an infinite loop.
-
-    #= Vectorized
-    c_matrix = [exp(z_grid[i]) .+ (1+r)*a_grid[j] .- a_grid[k] for j in 1:length(a_grid), i in 1:length(z_grid), k in 1:length(a_grid)] # A three-dimensional array of consumptions, containing all possible consumption values, for all possible combinations of k, z and k'.
-    u_matrix = u.(c_matrix) # A three-dimensional array of utility values. All possible values for the utility function, for all possible combinations of k, z and k'. 
-    
-    @time begin 
-        
-        while maximum(abs.(V_prev - V)) > tol && iter < maxiter 
-                
-            V_prev = copy(V); 
-    
-            EV = [Π[j,:]' * V_prev[n,:] for n in 1:length(a_grid), j in 1:length(z_grid)] # A matrix of expected values for each possible combination of k' and z.
-    
-            # We start the iterative process.
-            @threads for j in 1:length(z_grid) # Given z...
-                @threads for i in 1:length(a_grid) # Given k...
-                    value = u_matrix[i,j,:] + β * EV[:,j] # All possible values for the Bellman equation, given k and z.
-                    V[i,j] = maximum(value); # Takes the maximum value. 
-                    policy_index[i,j] = argmax(value) # Capital policy function indexes. Will be necessary for calculating Euler Equation Errors in the future. 
-                    policy[i,j] = a_grid[policy_index[i,j]] # Capital policy function.
-                end
-            end 
-    
-            iter += 1; # Adds one to the iteration counter.
-            # print("\n", "Iter: ", iter)
-            # print("\n", "Distance: ", maximum(abs.(V_prev - V)))
-        end
-        # print("Total iterations: ", iter, "\n")
-    end
-    =# 
 
     n_h = 10 # Number of "additional iterations", without maximization, that will be performed using the approximated
     # policy function, after each brute force maximization. I set n_h = 10, as suggested by the exercise. 
@@ -174,42 +126,53 @@ function solve_individuals_problem(r)
             end
     
         end
-        # print("Total iterations: ", iter, "\n")
+        print("Total iterations: ", iter, "\n")
     end
+ 
+    # Finding the stationary distribution. See L&S RMT pg. 788 for details.
 
-    unstacked_x = [[a_grid[i], z_grid[h]] for h in eachindex(z_grid), i in eachindex(a_grid)] # Unstacked
-    x = vec(unstacked_x) # Stacked 
+    # Mapping (a, s) states into a single state vector, x. In practice, it will not be necessary. Just replicating what is written in the book for a better understanding.
+    # unstacked_x = [[a_grid[i], z_grid[h]] for h in eachindex(z_grid), i in eachindex(a_grid)]; # Unstacked
+    # x = vec(unstacked_x); # Stacked 
 
-    # Induced Markov Chain: 
-    indicator(ap, a, s) = 1*(a_grid[ap] == policy[a,s])
+    # Induced Markov Chain (the optimal policy function a' = g(a, s) and the Markov chain P on s induce a Markov chain for x via the formula I(a', a, s)Π(s, s'). 
     print("Computing the induced Markov chain... \n")
-    Π_x = dropzeros(sparse(blockmatrix_to_matrix([[indicator(ap, a, s)*Π[s, sp] for a in eachindex(a_grid), ap in eachindex(a_grid)] for s in eachindex(z_grid), sp in eachindex(z_grid)])))
+    Πs = kron(Π,ones(length(a_grid),1));
+    indic = vcat([(a_grid .== policy[i]) for i in 1:(length(a_grid)*length(z_grid))]'...) # Indicator 
+    Π_x = sparse(vcat([kron(Πs[s,:], indic[s,:]) for s in 1:(length(a_grid)*length(z_grid))]'...)) # Induced Markov chain
 
+    # The stationary distribution is given by the eigenvector of Π_x associated with its unit eigenvalue. There is a quick way to get this eigenvector directly: 
     print("Obtaining the eigenvector... \n")
-    v = [1; (I - Π_x'[2:end, 2:end]) \ Vector(Π_x'[2:end, 1])] # Directly solving 
-    norm_π = v./sum(v)
+    v = [1; (I - Π_x'[2:end, 2:end]) \ Vector(Π_x'[2:end, 1])]; # Directly solving 
+    norm_π = v./sum(v); # Normalizing to add 1.
 
     print("Computing the stationary distribution... \n")
-    π_∞ = reshape(norm_π, length(a_grid), length(z_grid)) # stationary distribution.
+    π_∞ = reshape(norm_π, length(a_grid), length(z_grid)); # Stationary distribution.
 
     # Marginal asset distribution: 
-    mg_π = zeros(length(a_grid))
+    mg_π = zeros(length(a_grid));
 
     for i in eachindex(z_grid)
-        mg_π = mg_π + π_∞[:, i]
+        mg_π = mg_π + π_∞[:, i];
     end 
 
+    # Aggregate savings: 
     agg_savings = norm_π'vec(policy);
     print("Aggregate savings: ", agg_savings, "\n")
+
     return agg_savings, V, policy, policy_index, π_∞, mg_π
 end
+
+###########
+## b = 3 ## 
+########### 
 
 # Parameters: 
 
 β = 0.96;
 γ = 3;
 ρ = 0.2;
-σ = 0.4;
+σ = 0.4*sqrt(1-ρ^2);
 N_z = 7; 
 m = 3;
 
@@ -235,10 +198,9 @@ r = 1/β - 1; # Risk-free rate.
 
 # Generating the asset grid: 
 
-grid_size = 500;
 a_min = -3;
 a_max = 16;
-a_grid = range(a_min, a_max, 200); # Note that a small positive perturbation to the lower bound is required so that agents are not able to borrow infinitely.
+a_grid = range(a_min, a_max, 100); # Note that a small positive perturbation to the lower bound is required so that agents are not able to borrow infinitely.
 
 # Aggregate savings plot (E[a(r)]):
 grid_r = range(0, 1/β - 1, 300)
@@ -247,30 +209,75 @@ grid_r = range(0, 1/β - 1, 300)
     Ears = solve_individuals_problem.(grid_r)
 end 
 
+###########
+## b = 6 ## 
+###########
+
+# Parameters: 
+
+β = 0.96;
+γ = 3;
+ρ = 0.2;
+σ = 0.4*sqrt(1-ρ^2);
+N_z = 7; 
+m = 3;
+
+# Discretizing z: 
+z_grid, Π = tauchen(0, σ^2, ρ, N_z, m);
+
+# Utility function: 
+
+function u(c) 
+    if c > 0
+        u = (c^(1-γ) - 1)/(1-γ); # Utility function
+    else 
+        u = -Inf # This is necessary to avoid negative consumption. If c < 0, utility = -∞.
+    end
+    return u
+end
+
+z_min = z_grid[1]
+z_max = z_grid[N_z]
+
+r = 1/β - 1; # Risk-free rate. 
+ϕ = exp(z_min)/r; # Natural debt limit.
+
+# Generating the asset grid: 
+
+a_min = -6;
+a_max = 16;
+a_grid = range(a_min, a_max, 100); # Note that a small positive perturbation to the lower bound is required so that agents are not able to borrow infinitely.
+
+# Aggregate savings plot (E[a(r)]):
+grid_r = range(0, 1/β - 1, 500)
+
 @time begin 
     Ears_2 = solve_individuals_problem.(grid_r)
 end 
 
+##############
+## Plotting ##
+##############
+
 Ear = [Ears[i][1] for i in eachindex(grid_r)]
 Ear_2 = [Ears_2[i][1] for i in eachindex(grid_r)]
-b1 = [-3 for i in eachindex(grid_r)]
-b2 = [-6 for i in eachindex(grid_r)]
-plot(Ear, grid_r)
+plot(Ear, grid_r, legend=false)
 plot!(Ear_2, grid_r)
-plot!(b1, grid_r, color="black", linestyle=:dash)
-plot!(b2, grid_r, color="black", linestyle=:dash)
 hline!([1/β - 1], color="red", linestyle=:dash)
 vline!([0], color="black", linestyle=:dash)
 # savefig("fig1863.png")
 
-########## Eq.:
+#################
+## Equilibrium ##
+#################
 
 @time begin
     r = find_zero(solve_individuals_problem, (0, 1/β - 1), Bisection())
 end  
 
-
-r
+#################
+## Other plots ##
+#################
 
 agg_savings, V, policy, policy_index, π_∞, mg_π = solve_individuals_problem(r)
 
@@ -290,10 +297,10 @@ plot(repeat(a_grid,1,7), repeat(z_grid',length(a_grid)), π_∞,
     xlab = "a", 
     ylab = "z", 
     zlab = "π",
-    seriestype=:surface,)
+    seriestype=:surface)
 
 
 # Marginal asset distribution: 
 plot(a_grid, π_∞) # Stationary distribution
 plot(a_grid, mg_π) # Marginal asset distribution plot.
-savefig("fig1872.png")
+# savefig("fig1872.png")
